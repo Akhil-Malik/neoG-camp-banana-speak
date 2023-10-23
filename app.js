374dcd2fd02f40cfa9afaee05218c739ccc27900
Akhil <akhilmalik720@gmail.com>
2023-10-23 17:04:13 +0530

    input output processing done

---
+++ app.js
@@ -0,0 +1,7 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+console.log(txtInput)
+btnTranslate.addEventListener("click", function clickEventHandler(){
+   outputDiv.innerText = "hyshysh " + txtInput.value;
+})
